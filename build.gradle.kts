b7cf03f1720934f3564cead97f3db3ebbc3c0950
Yuriy Kulikov <Yuriy.Kulikov.87@gmail.com>
2020-09-04 01:47:40 +0200

    alpha-02

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val kotlin = "1.4.0"
-    val compose = "1.0.0-alpha01"
+    val compose = "1.0.0-alpha02"
 
     allprojects {
         extra.apply {
@@ -17,7 +17,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha08")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha09")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin")
         classpath("com.google.gms:google-services:4.3.3")
     }
