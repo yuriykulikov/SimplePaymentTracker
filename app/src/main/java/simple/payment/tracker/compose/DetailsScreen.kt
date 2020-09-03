c46d501b14e1a06947cb664fc5066dbd886262b0
Yuriy Kulikov <Yuriy.Kulikov.87@gmail.com>
2020-09-04 01:47:59 +0200

    Some changes in the details

--- app/src/main/java/simple/payment/tracker/compose/DetailsScreen.kt
+++ app/src/main/java/simple/payment/tracker/compose/DetailsScreen.kt
@@ -10,17 +10,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Divider
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.state
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawOpacity
@@ -28,12 +28,10 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.ui.tooling.preview.Preview
 import org.koin.core.context.KoinContextHandler
 import simple.payment.tracker.Payment
 import simple.payment.tracker.PaymentsRepository
 import simple.payment.tracker.Transaction
-import simple.payment.tracker.theme.PaymentsTheme
 import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.Date
@@ -44,74 +42,6 @@ private val dateFormat = SimpleDateFormat(
   Locale.GERMANY
 )
 
-@Composable
-@Preview("DetailsScreen preview")
-fun PreviewDetailsScreen() {
-  PaymentsTheme {
-    Surface {
-      val transaction = Transaction(
-        Payment(
-          category = "Baby",
-          comment = "Comment",
-          merchant = "Amazon",
-          notificationId = null,
-          time = 100500L,
-          sum = 101,
-          cancelled = false,
-          trip = null
-        )
-      )
-      DetailsScreen(
-        transaction,
-        mutableStateOf(Screen.Details(transaction))
-      )
-    }
-  }
-}
-
-class DetailsScreenState(
-  val category: MutableState<String?>,
-  val cancelled: MutableState<Boolean>,
-  val comment: MutableState<TextFieldValue>,
-  val sum: MutableState<TextFieldValue>,
-  val merchant: MutableState<TextFieldValue>,
-  val trip: MutableState<TextFieldValue>,
-  val time: MutableState<TextFieldValue>
-) {
-  companion object {
-    @Composable
-    fun create(): DetailsScreenState {
-      return DetailsScreenState(
-        category = state { null },
-        cancelled = state { false },
-        comment = state { TextFieldValue("") },
-        sum = state { TextFieldValue("") },
-        merchant = state { TextFieldValue("") },
-        trip = state { TextFieldValue("") },
-        time = state {
-          TextFieldValue(dateFormat.format(Date.from(Instant.now())))
-        }
-      )
-    }
-
-    @Composable
-    fun fromTransaction(transaction: Transaction): DetailsScreenState {
-      return DetailsScreenState(
-        category = state { transaction.category },
-        cancelled = state { transaction.cancelled },
-        comment = state { TextFieldValue(transaction.comment) },
-        sum = state { TextFieldValue(transaction.sum.toString()) },
-        merchant = state { TextFieldValue(transaction.merchant) },
-        trip = state { TextFieldValue(transaction.trip ?: "") },
-        time = state {
-          val initialTime = transaction.time.let { Instant.ofEpochMilli(it) }
-          TextFieldValue(dateFormat.format(Date.from(initialTime)))
-        }
-      )
-    }
-  }
-}
-
 /**
  * Transaction can be with a notification or without
  */
@@ -120,49 +50,71 @@ fun DetailsScreen(
   transaction: Transaction?,
   currentScreen: MutableState<Screen>
 ) {
-  val state =
-    if (transaction != null) DetailsScreenState.fromTransaction(transaction) else DetailsScreenState.create()
+  val category: MutableState<String?> = remember {
+    mutableStateOf(transaction?.category)
+  }
+  val sum: MutableState<TextFieldValue> = remember {
+    mutableStateOf(TextFieldValue(transaction?.sum?.toString() ?: ""))
+  }
+  val merchant: MutableState<TextFieldValue> = remember {
+    mutableStateOf(TextFieldValue(transaction?.merchant ?: ""))
+  }
+  val time: MutableState<TextFieldValue> = remember {
+    val initialTime = transaction?.time?.let { Instant.ofEpochMilli(it) } ?: Instant.now()
+    mutableStateOf(
+      TextFieldValue(dateFormat.format(Date.from(initialTime)))
+    )
+  }
+
+  val cancelled = remember { mutableStateOf(transaction?.cancelled ?: false) }
+  val comment = remember { mutableStateOf(TextFieldValue(transaction?.comment ?: "")) }
+  val trip = remember { mutableStateOf(TextFieldValue(transaction?.trip ?: "")) }
+
+  val strike = MaterialTheme.typography.body1.copy(textDecoration = TextDecoration.LineThrough)
 
   Scaffold(
     topBar = {
       TopAppBar(
         title = {
-          with(state) {
-            Text(
-              text = "${sum.value.text} to ${merchant.value.text}",
-              style =
-              MaterialTheme.typography.body1.copy(
-                textDecoration = if (cancelled.value) TextDecoration.LineThrough else TextDecoration.None
-              )
-            )
-          }
+          Text(
+            text = "${sum.value.text} to ${merchant.value.text} ${if (cancelled.value) " (cancelled)" else ""}",
+            style = if (cancelled.value) strike else MaterialTheme.typography.body1
+          )
         },
         actions = {
-          Actions(
-            state, save = {
-              with(state) {
-                KoinContextHandler.get().get<PaymentsRepository>()
-                  .changeOrCreatePayment(
-                    transaction?.id,
-                    Payment(
-                      notificationId = transaction?.payment?.notificationId
-                        ?: transaction?.notification?.time,
-                      time = transaction?.notification?.time
-                        ?: requireNotNull(
-                          dateFormat.parse(time.value.text)
-                        ).time,
-                      category = category.value!!,
-                      comment = comment.value.text,
-                      merchant = merchant.value.text,
-                      sum = sum.value.text.toInt(),
-                      cancelled = cancelled.value,
-                      trip = trip.value.text.let { if (it.isEmpty()) null else it }
-                    )
+          val canSave = (runCatching<Date?> { dateFormat.parse(time.value.text) }.isSuccess
+            && sum.value.text.toIntOrNull() != null
+            && merchant.value.text.isNotEmpty()
+            && category.value != null)
+          IconButton(onClick = {
+            if (canSave) ({
+              KoinContextHandler.get().get<PaymentsRepository>()
+                .changeOrCreatePayment(
+                  transaction?.id,
+                  Payment(
+                    notificationId = transaction?.payment?.notificationId
+                      ?: transaction?.notification?.time,
+                    time = transaction?.notification?.time
+                      ?: requireNotNull(
+                        dateFormat.parse(time.value.text)
+                      ).time,
+                    category = category.value!!,
+                    comment = comment.value.text,
+                    merchant = merchant.value.text,
+                    sum = sum.value.text.toInt(),
+                    cancelled = cancelled.value,
+                    trip = trip.value.text.let { if (it.isEmpty()) null else it }
                   )
-              }
-            },
-            currentScreen = currentScreen
-          )
+                )
+              currentScreen.value = Screen.List
+            })()
+          }) {
+            Text(
+              text = "Save",
+              style = MaterialTheme.typography.body2,
+              modifier = Modifier.drawOpacity(if (canSave) 1f else 0.2f)
+            )
+          }
         }
       )
     },
@@ -171,68 +123,50 @@ fun DetailsScreen(
         Column {
           ScrollableColumn {
             Column {
-              TextInputs(state, transaction?.notification != null)
-              CategorySelector(state.category)
+              Row {
+                NamedTextFieldInput(
+                  header = "€",
+                  state = sum,
+                  keyboardType = KeyboardType.Number,
+                  modifier = Modifier.fillMaxWidth(0.8f).padding(horizontal = 16.dp),
+                  onValueChange = {
+                    if (it.text.toIntOrNull() != null || it.text.isEmpty()) {
+                      sum.value = it
+                    }
+                  }
+                )
+
+                Checkbox(
+                  modifier = Modifier.debugBorder().padding(16.dp),
+                  checked = !cancelled.value,
+                  onCheckedChange = { cancelled.value = !cancelled.value }
+                )
+              }
+            }
+            Column {
+              NamedTextFieldInput(header = "to", state = merchant)
+              NamedTextFieldInput(header = "for", state = comment)
+              Row {
+                NamedTextFieldInput(
+                  header = "on",
+                  state = time,
+                  enabled = transaction?.notification == null,
+                  modifier = Modifier.weight(0.5f).padding(start = 16.dp),
+                )
+                NamedTextFieldInput(
+                  header = "Trip",
+                  state = trip,
+                  modifier = Modifier.weight(0.5f).padding(end = 16.dp).padding(start = 8.dp),
+                )
+              }
+              InputDivider()
+              CategorySelector(category)
             }
           }
         }
       }
     }
   )
-}
-
-@Composable
-private fun Actions(
-  state: DetailsScreenState,
-  currentScreen: MutableState<Screen>,
-  save: () -> Unit
-) {
-  IconButton(onClick = { state.cancelled.value = !state.cancelled.value }) {
-    Text(
-      text = if (state.cancelled.value) "X" else "X",
-      style = MaterialTheme.typography.body2
-    )
-  }
-
-  val canSave = runCatching { dateFormat.parse(state.time.value.text) }.isSuccess
-    && state.sum.value.text.toIntOrNull() != null
-    && state.merchant.value.text.isNotEmpty()
-    && state.category.value != null
-  IconButton(onClick = {
-    if (canSave) {
-      currentScreen.value = Screen.List
-      save()
-    }
-  }) {
-    Text(
-      text = "Save",
-      style = MaterialTheme.typography.body2,
-      modifier = Modifier.drawOpacity(if (canSave) 1f else 0.2f)
-    )
-  }
-}
-
-/** Inputs for a new payment */
-@Composable
-private fun TextInputs(
-  state: DetailsScreenState,
-  fromNotfication: Boolean
-) {
-  NamedTextFieldInput(
-    header = "€",
-    state = state.sum,
-    keyboardType = KeyboardType.Number,
-    onValueChange = {
-      if (it.text.toIntOrNull() != null || it.text.isEmpty()) {
-        state.sum.value = it
-      }
-    }
-  )
-  NamedTextFieldInput(header = "to", state = state.merchant)
-  NamedTextFieldInput(header = "for", state = state.comment)
-  NamedTextFieldInput(header = "on", state = state.time, enabled = !fromNotfication)
-  NamedTextFieldInput(header = "Trip", state = state.trip)
-  InputDivider()
 }
 
 @Composable
@@ -249,6 +183,7 @@ private fun NamedTextFieldInput(
   state: MutableState<TextFieldValue>,
   keyboardType: KeyboardType = KeyboardType.Text,
   enabled: Boolean = true,
+  modifier: Modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
   onValueChange: (TextFieldValue) -> Unit = { state.value = it }
 ) {
   OutlinedTextField(
@@ -261,9 +196,9 @@ private fun NamedTextFieldInput(
     value = state.value,
     onValueChange = if (enabled) onValueChange else { _ -> },
     keyboardType = keyboardType,
-    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-    textStyle = MaterialTheme.typography.body1
-    //, backgroundColor = Color.Transparent
+    modifier = modifier,
+    textStyle = MaterialTheme.typography.body1,
+    //backgroundColor = Color.Transparent,
   )
 }
 
