026db12cbf8d90ef85e25261e2d738baca92928e
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2023-04-02 14:19:33 +0200

    feat: amazon 2023

--- app/src/test/java/simple/payment/tracker/AmazonPaymentsParsing.kt
+++ app/src/test/java/simple/payment/tracker/AmazonPaymentsParsing.kt
@@ -1,10 +1,10 @@
 package simple.payment.tracker
 
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AmazonPaymentsParsing {
   @Test
@@ -37,27 +37,31 @@ class AmazonPaymentsParsing {
             loader.getResource("Amazon - 2020.tsv"),
             loader.getResource("Amazon - 2021.tsv"),
             loader.getResource("Amazon - 2022.tsv"),
+            loader.getResource("Amazon - 2023.tsv"),
         )
         .map { it.readText(charset = Charsets.UTF_8) }
         .flatMap {
           val lines = it.lines()
           val indexOfSum = lines.first().split("\t").indexOf("sum")
-          lines.drop(1).mapNotNull { line -> parseLine(line, indexOfSum) }
+          val indexOfTransfer = lines.first().split("\t").indexOf("transfer")
+          lines.drop(1).mapNotNull { line -> parseLine(line, indexOfSum, indexOfTransfer) }
         }
   }
 
-  private fun parseLine(line: String, indexOfSum: Int): AmazonPayment? {
+  private fun parseLine(line: String, indexOfSum: Int, indexOfTransfer: Int): AmazonPayment? {
     val split = line.split("\t")
     val orderId = split[0]
     val dateText = split[3]
     val (sumText, category, comment, user) = split.subList(indexOfSum, indexOfSum + 4)
     val sum = sumText.substringBefore(",").toInt()
+    val refund = split.getOrNull(indexOfSum + 5)?.substringBefore(",")?.toIntOrNull() ?: 0
+    val transfer = split.getOrNull(indexOfTransfer)?.substringBefore(",")?.toIntOrNull() ?: 0
     val date = dateFormats.firstNotNullOf { runCatching { it.parse(dateText) }.getOrNull() }
     return if (sum != 0) {
       AmazonPayment(
           category = category,
           time = date.time,
-          sum = sum,
+          sum = sum - refund - transfer,
           comment = comment,
           orderId = orderId,
           user = user)
