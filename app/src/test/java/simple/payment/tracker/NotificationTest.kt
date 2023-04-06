f41b151c73ec16d9383ab206cad4d4956d2b7827
Yuriy Kulikov <827794+yuriykulikov@users.noreply.github.com>
2023-04-06 18:19:01 +0200

    Revert "Update app/src/test/java/simple/payment/tracker/NotificationTest.kt"
    This reverts commit b72e13a6aea9469df5b2712c5bafe4d0b52fa422.

--- app/src/test/java/simple/payment/tracker/NotificationTest.kt
+++ app/src/test/java/simple/payment/tracker/NotificationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class NotificationTest {
   @Test
   fun parse() {
-    Notification(0, "You sent 290.00 € EUR to Someone Someone.", "Pixel 4a").sumCents() shouldBe
+    Notification(0, "You sent 290.00 € EUR to Marcus Feustel.", "Pixel 4a").sumCents() shouldBe
         29000
   }
 }
